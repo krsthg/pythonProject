32c107cb42b666549f0019a4c74b2286ece2447b
wlswodnd <83488101+wlswodnd@users.noreply.github.com>
2021-10-10 17:49:15 +0900

    2021-10-10

--- index/templates/f/A.py
+++ index/templates/f/A.py
@@ -1,5 +1,5 @@
 words = []
-with open('줄임말1.txt','r', encoding='utf-8') as f:
+with open('../줄임말1.txt', 'r', encoding='utf-8') as f:
     info = f.readlines()
     for i in info:
         if i[0] =='[':
